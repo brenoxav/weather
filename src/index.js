0e0530a4f0966ad8096c9dca49a1ea1a904630f9
Breno Xavier <brenoxav@gmail.com>
2021-05-14 18:21:17 -0400

    Add weather icons, fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import "./assets/css/style.css";
+import './assets/css/style.css';
 
 const loader = document.querySelector('.loader');
+const backdrop = document.querySelector('.backdrop');
+const warningMessage = document.querySelector('.warning-message');
+
 const searchInput = document.querySelector('.search-input');
 const searchCityBtn = document.querySelector('.search-city-btn');
 const searchHereBtn = document.querySelector('.search-here-btn');
@@ -11,95 +14,103 @@ const unitC = document.querySelector('.unit-c');
 const unitF = document.querySelector('.unit-f');
 
 const locationName = document.querySelector('.location-name');
+const weatherIcon = document.querySelector('.weather-icon');
 const weatherDescription = document.querySelector('.weather-description');
 const temperatureValue = document.querySelector('.temperature-value');
 const temperatureUnit = document.querySelectorAll('.temperature-unit');
 const minTemperature = document.querySelector('.min-temperature');
 const maxTemperature = document.querySelector('.max-temperature');
 
+const ICON_URL = 'https://raw.githubusercontent.com/brenoxav/project-assets/gh-pages/weather-icons/';
 const API_KEY = 'e24777d55aaff3d179ae147424695333';
 
-function getWeather(input) {
-  loader.classList.add('display');
-  let url = '';
-  if(typeof input === 'string'){
-    url = `http://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=${API_KEY}`;
+function convertToUnit(tempC) {
+  if (unitCheckbox.checked) {
+    temperatureUnit.forEach((unit) => { unit.textContent = '°C'; });
+    return tempC;
   }
-  else if(typeof input === 'object') {
-    url = `http://api.openweathermap.org/data/2.5/weather?lat=${input.lat}&lon=${input.lon}&units=metric&appid=${API_KEY}`
-  }
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      loader.classList.remove('display');
-      console.log(data);
-      renderWeather(data);
-    })
-    .catch(err => {
-      console.log(err);
-      locationName.textContent = "Location not found!"
-    });
+  temperatureUnit.forEach((unit) => { unit.textContent = '°F'; });
+  return (((tempC * 9) / 5) + 32);
 }
-
-searchInput.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') {
-    getWeather(searchInput.value)
-  }
-})
-
-searchCityBtn.addEventListener('click', () => {
-  getWeather(searchInput.value)
-});
-
-function getCurrentLocation() {
-  loader.classList.add('display');
-  const options = {
-    timeout: 5000
-  }
-  //if(navigator.geolocation)
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject, options);
-  })  
-}
-
-searchHereBtn.addEventListener('click', () => {
-  getCurrentLocation()
-    .then(geo => getWeather({lat: geo.coords.latitude, lon: geo.coords.longitude}))
-    .catch(err => {
-      console.error(`ERROR GETTING COORDINATES: ${err.code}: ${err.message}`)
-      loader.classList.remove('display');
-      locationName.textContent = "Location service is not available for this browser. Try searching by the city name."
-    });
-});
 
 function renderWeather(data) {
   locationName.textContent = `${data.name}, ${data.sys.country}`;
+  weatherIcon.style.backgroundImage = `url('${ICON_URL}${data.weather[0].icon}.svg')`;
   weatherDescription.textContent = data.weather[0].description;
   temperatureValue.textContent = `${Math.round(convertToUnit(data.main.temp))}`;
   minTemperature.textContent = `${Math.round(convertToUnit(data.main.temp_min))}`;
   maxTemperature.textContent = `${Math.round(convertToUnit(data.main.temp_max))}`;
 }
 
-function convertToUnit(tempC) {
-
-  if(unitCheckbox.checked) {
-    temperatureUnit.forEach(unit => unit.textContent = "°C");
-    return tempC
+function getWeather(input) {
+  warningMessage.innerHTML = '';
+  backdrop.classList.add('display');
+  loader.classList.add('display');
+  let url = '';
+  if (typeof input === 'string') {
+    url = `http://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=${API_KEY}`;
+  } else if (typeof input === 'object') {
+    url = `http://api.openweathermap.org/data/2.5/weather?lat=${input.lat}&lon=${input.lon}&units=metric&appid=${API_KEY}`;
   }
-  else {
-    temperatureUnit.forEach(unit => unit.textContent = "°F");
-    return ((tempC * 9/5) + 32);
-  }
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      backdrop.classList.remove('display');
+      loader.classList.remove('display');
+      renderWeather(data);
+    })
+    .catch(() => {
+      backdrop.classList.add('display');
+      warningMessage.innerHTML = `
+      <p class="warning-message">LOCATION NOT FOUND</p>
+      </br>
+      <p class="warning-message">Click the pin icon above to use you current location OR type a city name in the search field.</p>
+      `;
+    });
 }
 
+searchInput.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    getWeather(searchInput.value);
+  }
+});
+
+searchCityBtn.addEventListener('click', () => {
+  getWeather(searchInput.value);
+});
+
+function getCurrentLocation() {
+  backdrop.classList.add('display');
+  loader.classList.add('display');
+  const options = {
+    timeout: 5000,
+  };
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject, options);
+  });
+}
+
+searchHereBtn.addEventListener('click', () => {
+  getCurrentLocation()
+    .then((geo) => getWeather({ lat: geo.coords.latitude, lon: geo.coords.longitude }))
+    .catch(() => {
+      backdrop.classList.add('display');
+      loader.classList.remove('display');
+      warningMessage.innerHTML = `
+      <p class="warning-message">LOCATION SERVICE IS NOT AVAILABLE FOR THIS BROWSER</p>
+      </br>
+      <p class="warning-message">Either give permission for this site to use your geolocation OR use the search field.</p>
+      `;
+    });
+});
+
 unitSwitch.addEventListener('click', () => {
-  if(unitCheckbox.checked) {
+  if (unitCheckbox.checked) {
     unitC.classList.add('active-unit');
     unitF.classList.remove('active-unit');
-  }
-  else {
+  } else {
     unitC.classList.remove('active-unit');
     unitF.classList.add('active-unit');
   }
   getWeather(locationName.textContent);
-})
+});
